a964462e3ba997beb31fa9b8b3e0fe4358c26efc
Gokul Bijoy <81373116+carlmax-here@users.noreply.github.com>
2021-09-18 11:08:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -297,8 +297,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('MOVIE GROUP', url='https://t.me/CinemaPedikaGroup'),
+                        InlineKeyboardButton('SERIES GROUP', url='https://t.me/SERIES4USGroup')
                     ]
                     ]
                 
@@ -329,8 +329,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('MOVIE GROUP ', url='https://t.me/CinemaPedikaGroup'),
+                        InlineKeyboardButton('SERIES GROUP', url='https://t.me/SERIES4USGroup')
                     ]
                     ]
                 
